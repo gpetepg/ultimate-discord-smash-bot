8b14d83c920a287f2294f5f3ffbffed8e6435bce
Ty <tylereguo@gmail.com>
2019-06-06 22:18:46 -0700

    Cleaning up the code

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # ultimate-discord-smash-bot
-Bot for my Smash Ultimate discord server.
+Bot we made for our Smash Ultimate discord server.
+
+Only dependancy is [`discord`](https://github.com/Rapptz/discord.py) but I've included a `requiements.txt` regardless. I'll probably add dismock which is mentioned below too for testing.
+
+You will need to provide your own servers token imported from `credentials.py`.
+
+Basic setup using `bash`. I reccomend using a [virtual environment](https://docs.python.org/3/library/venv.html).
+
+	git clone git@github.com:gpetepg/ultimate-discord-smash-bot.git
+    python3 -m venv discord-ve
+    source discord-ve/bin/activate
+    pip install -r requirements.txt
+    python3 ultimate_bot.py
+
+Official discord api documentation:
+https://discordpy.readthedocs.io/en/latest/index.html
+
+NOTE:
+If you are using `python3.6` on `Mac OS X` There is a known SSL issue which will give you trouble when trying to authenticate with Discord. Run the shell scripts provided in `python3.6_ssl_error_scripts/` to address this.
+    
+    sh Install Certificates.command
+    sh Update Shell Profile.command
 
 TODO:
- - Instructions ( + Explain 3.6 error)
- - Add more functionality to the bot
+ - [Testing](https://github.com/DXsmiley/dismock)
--- ultimate_discord_smash_bot/ultimate_bot.py
+++ ultimate_discord_smash_bot/ultimate_bot.py
@@ -9,63 +9,12 @@ TOKEN = TOKEN
 
 client = discord.Client()
 
-@client.event
-async def on_message(message):
-    """ Messaged based events
-    """
-    
-    _introduction_channel_id = 521771464668610561
-    _verified_role_id = 585273441058553866
+######### Helper functions ##############
 
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith("!bot"):
-        greetings = f"""Hello {message.author.mention} this is our bot here are its utilities:
-
-!data <character name> : Returns a link to a character's data.
-!matchup <character 1> <character 2> : Returns a link with match up data between the two characters.
-"""
-        await message.channel.send(greetings)
-
-    # @ToDo Create this logic
-    if message.content.startswith("!matchup"):
-        matchup_message = display_matchup(message)
-        await message.channel.send(matchup_message)
-
-
-    if message.content.startswith("!data"):
-        # separate the !data from the message content and clear out any delimeters with spaces then check the list...
-        character = re.sub('[^a-zA-Z0-9]+', ' ', ' '.join(message.content.lower().split(" ")[1:]))
-        if character in characters:
-            character = "_".join(message.content.split(" ")[1:])
-            character_data = f"https://ultimateframedata.com/{character}.php".lower()
-            await message.channel.send(character_data)
-        else:
-            await message.channel.send("Could not find this character, please enter a valid character")
-
-    # Don't need to check if the user already has the "Verified" role, if they already have it nothing will happen.
-    if (message.channel == client.get_channel(_introduction_channel_id)) and (len(message.content) > 20 ):
-        role = discord.utils.get(message.guild.roles, id=_verified_role_id)
-        await message.author.add_roles(role)
-
-@client.event
-async def on_member_join(member):
-    """ When a member joins send them a DM.
-    """
-
-    msg = f"""Hello {member.display_name}. Welcome to our Smash Ultimate Discord.
-Please go to the #Introduction channel and in ONE MESSAGE please message with the following format here: 
-    - Name 
-    - Whose friend you are or who invited you 
-    - Nintendo friend code"""
-    await member.send(msg)
-
-def replace_spaces_with_dashes(message):
+def _replace_spaces_with_dashes(message):
     return message.replace(" ", "-")
 
-def display_matchup(message):
+def _display_matchup(message):
     message.content = message.content.replace("!matchup", "")
     foundChars = []
     for c in characters:
@@ -78,10 +27,71 @@ def display_matchup(message):
         return "You must provide only 2 characters!"
         return
     foundChars.insert(1, "vs")
-    endResult = map(replace_spaces_with_dashes, reversed(foundChars))
+    endResult = map(_replace_spaces_with_dashes, reversed(foundChars))
     endResult = "-".join(endResult)
     url = f"https://ssbworld.com/matchup/{endResult}"
     return url
+
+#########################################
+
+@client.event
+async def on_message(message):
+    """ Messaged based events"""
+    
+    _introduction_channel_id = 521771464668610561
+    _verified_role_id = 585273441058553866
+
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    # This displays the bot's options.
+    if message.content.startswith("!bot"):
+        greetings = f"""Hello {message.author.mention} this is our bot here are its utilities:
+
+!data <character name> : Returns a link to a character's data.
+!matchup <character 1> <character 2> : Returns a link with match up data between the two characters.
+"""
+        await message.channel.send(greetings)
+
+    # This displays match-up info
+    if message.content.startswith("!matchup"):
+        matchup_message = _display_matchup(message)
+        await message.channel.send(matchup_message)
+
+
+    # This displays framedata
+    if message.content.startswith("!data"):
+        # separate the !data from the message content and clear out any delimeters with spaces then check the list...
+        character = re.sub('[^a-zA-Z0-9]+', ' ', ' '.join(message.content.lower().split(" ")[1:]))
+        if character in characters:
+            character = "_".join(message.content.split(" ")[1:])
+            character_data = f"https://ultimateframedata.com/{character}.php".lower()
+            await message.channel.send(character_data)
+        else:
+            await message.channel.send("Could not find this character, please enter a valid character")
+
+    # Logic to check if the member wrote something in introduction and give them the verified role.
+    if (message.channel == client.get_channel(_introduction_channel_id)) and (len(message.content) > 20 ):
+    # Don't need to check if the user already has the "Verified" role, if they already have it nothing will happen.
+        role = discord.utils.get(message.guild.roles, id=_verified_role_id)
+        await message.author.add_roles(role)
+
+#########################################
+
+@client.event
+async def on_member_join(member):
+    """ When a member joining based events. """
+
+    # When a member joins tell them what to do.
+    msg = f"""Hello {member.display_name}. Welcome to our Smash Ultimate Discord.
+Please go to the #Introduction channel and in ONE MESSAGE please message with the following format here: 
+    - Name 
+    - Whose friend you are or who invited you 
+    - Nintendo friend code"""
+    await member.send(msg)
+
+#########################################
 
 @client.event
 async def on_ready():
